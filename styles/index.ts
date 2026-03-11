8a7a83049fce0b703bd69aa040c87c6829242d4a
Brandon Keepers <brandon@opensoul.org>
2026-03-11 08:58:32 -0400

    Add noaa charts using WMS

--- styles/index.ts
+++ styles/index.ts
@@ -1,8 +1,20 @@
 import googleEarth from "@/styles/google-earth.json";
+import noaaEcdis from "@/styles/noaa-ecdis.json";
+import noaaPaper from "@/styles/noaa-paper.json";
 import openseamap from "@/styles/openseamap.json";
 import type { StyleSpecification } from "@maplibre/maplibre-react-native";
 
 export default [
+  {
+    id: "noaa-ecdis",
+    name: "NOAA (ECDIS)",
+    style: noaaEcdis as unknown as StyleSpecification,
+  },
+  {
+    id: "noaa-paper",
+    name: "NOAA (Paper Chart)",
+    style: noaaPaper as unknown as StyleSpecification,
+  },
   {
     id: "openseamap",
     name: "OpenSeaMap",
