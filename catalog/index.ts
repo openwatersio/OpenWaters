7e2c3c2dfec85743b482de1a52982de9cd123e3b
Brandon Keepers <brandon@opensoul.org>
2026-04-14 09:08:46 -0400

    Add Njord to the catalog

--- catalog/index.ts
+++ catalog/index.ts
@@ -1,5 +1,6 @@
 import type { CatalogEntry } from "./types";
 import googleEarth from "./google-earth.json";
+import njord from "./njord.json";
 import noaaRaster from "./noaa-raster";
 import openseamap from "./openseamap.json";
 import sentinel2Cloudless from "./sentinel-2-cloudless.json";
@@ -8,5 +9,5 @@ import vectorcharts from "./vectorcharts.json";
 export type { CatalogEntry, CatalogSource, CatalogSourceType, StyleSource, RasterSource, MBTilesSource, PMTilesSource } from "./types";
 
 export default async function loadCatalog(): Promise<CatalogEntry[]> {
-  return [await noaaRaster(), openseamap, googleEarth, sentinel2Cloudless, vectorcharts] as CatalogEntry[];
+  return [await noaaRaster(), njord, openseamap, googleEarth, sentinel2Cloudless, vectorcharts] as CatalogEntry[];
 }
